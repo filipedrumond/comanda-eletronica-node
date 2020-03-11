0eb4f3a21e6c4183d89b0d83103eb7074d9527bb
Filipe Drumond Pereira <filipe.dp@outlook.com>
2020-03-11 18:10:03 -0300

    Mostrando linha real no dev-tools

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,10 @@
 const mix = require('laravel-mix');
 
-mix.autoload({jquery: ['$', 'window.jQuery']});
+mix.autoload({ jquery: ['$', 'window.jQuery'] });
 
 mix.copy('src/assets', 'html/assets');
 mix.copy('db/img', 'html/assets/img');
 
 
-mix.js('src/main.js', 'html/build/js')
-    .sass('src/main.scss', 'html/build/css');
+mix.sass('src/main.scss', 'html/build/css')
+    .js('src/main.js', 'html/build/js').sourceMaps()

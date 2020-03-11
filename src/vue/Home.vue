325949ce1bc32f89f8967dd15175b290246ac132
Filipe Drumond Pereira <filipe.dp@outlook.com>
2020-03-11 18:09:42 -0300

    Passando dados entre os componentes

--- src/vue/Home.vue
+++ src/vue/Home.vue
@@ -2,9 +2,9 @@
     <div class="">
         <h1>I'M here</h1>
         {{ pratos }}
-        <teste />
-        <teste />
-        <teste />
+        <teste v-for="prato in pratos2" v-bind:key="prato.id" :prato="prato">
+        </teste>
+        {{ pratos2 }}
     </div>
 </template>
 
@@ -15,14 +15,21 @@ export default {
     name: "home",
     data() {
         return {
-            pratos: []
+            pratos: [],
+            pratos2: []
         };
     },
     created: function() {
-        console.log(this.$http.get("http://localhost:510/categorias"));
-        this.$http.get("http://localhost:510/categorias").then(response => {
-            this.pratos = response.body;
-        });
+        console.log("G");
+        this.$http
+            .get("http://localhost:510/categorias")
+            .then(response => {
+                this.pratos = response.body;
+                return this.$http.get("http://localhost:510/categorias");
+            })
+            .then(response => {
+                this.pratos2 = response.body;
+            });
     },
     components: { teste }
 };
--- src/vue/components/Teste.vue
+++ src/vue/components/Teste.vue
@@ -1,15 +1,27 @@
 <template>
-  <div>
-      maybe i'm here
-  </div>
+    <div>
+        maybe i'm here
+        {{ prato }}
+    </div>
 </template>
 
 <script>
 export default {
     name: "teste",
-}
+    data() {
+        return {
+            identidade: {}
+        };
+    },
+    props: ["prato"],
+    created: function() {},
+    methods: {
+        print: _ => {
+            console.log("dddd");
+        }
+    }
+};
 </script>
 
 <style>
-
 </style>
